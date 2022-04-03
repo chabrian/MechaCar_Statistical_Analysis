2db27532dda5ab99fe9dbeab97ce14c9a028a91d
briancha <briancha98@gmail.com>
2022-04-03 19:45:03 -0400

    adding analysis updates

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,13 @@ total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=medi
 
 # create the lot summary table
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups="keep")
+
+# DELIVERABLE 3
+
+# t-test for all lots against population mean
+t.test(suspension_coil_table$PSI,mu=1500)
+
+# t-test for each lot against population mean
+t.test(subset(suspension_coil_table$PSI,suspension_coil_table$Manufacturing_Lot == "Lot1"), mu=1500)
+t.test(subset(suspension_coil_table$PSI,suspension_coil_table$Manufacturing_Lot == "Lot2"), mu=1500)
+t.test(subset(suspension_coil_table$PSI,suspension_coil_table$Manufacturing_Lot == "Lot3"), mu=1500)
