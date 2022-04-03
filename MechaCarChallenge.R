5fd9f028c50e239ac0e552880968a8c2a2d3ca25
briancha <briancha98@gmail.com>
2022-04-03 19:30:53 -0400

    adding analysis updates

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,7 @@
 library(dplyr)
 
+# DELIVERABLE 1
+
 # load in data
 mpg_df <- read.csv('Resources/MechaCar_mpg.csv', stringsAsFactors = F)
 
@@ -8,3 +10,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_df))
+
+# DELIVERABLE 2
+
+# load in data
+suspension_coil_table <- read.csv('Resources/Suspension_Coil.csv', stringsAsFactors = F)
+
+# create the total summary table
+total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# create the lot summary table
+lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups="keep")
